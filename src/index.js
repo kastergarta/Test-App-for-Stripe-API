dc6267d876afb16d3caaaee1c2b9e2713030269f
Dennis Basyrov <dennis.basyrov@gmail.com>
2019-09-30 18:06:09 -0400

    red car test app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+toast.configure();
+
+function App() {
+  const [product] = React.useState({
+    name: "Tesla Roadster",
+    price: 64998.67,
+    description: "Cool car"
+  });
+
+  async function handleToken(token, addresses) {
+    console.log({token, addresses})
+    const response = await axios.post(
+      "http://localhost:3007/checkout",
+      { token, product }
+    );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="product">
+        <h1>{product.name}</h1>
+        <h3>On Sale · ${product.price}</h3>
+      </div>
+      <StripeCheckout
+        stripeKey="pk_test_reJ1gPlT5b6yX5bGa5byalsA000zHz5Qqq"
+        token={handleToken}
+        amount={product.price * 100}
+        name="Tesla Roadster"
+        billingAddress
+        shippingAddress
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
